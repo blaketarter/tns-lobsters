182e667560f821e9d79259090b1a71ac7961a792
Blake Tarter <blake@codekoalas.com>
2016-01-07 20:25:43 -0600

    renamed things so it makes more sense

--- hooks/after-prepare/android-name.js
+++ hooks/after-prepare/android-name.js
@@ -1,12 +1,15 @@
 var fs = require('fs');
 var xml2js = require('xml2js');
 
+var namePath = '/../../name.txt';
+var xmlPath = '/../../platforms/android/src/main/res/values/strings.xml';
+
 module.exports = function($logger) {
   return new Promise(function(resolve, reject) {
-    var nameString = fs.readFileSync(__dirname + '/../../name.txt').toString();
+    var nameString = fs.readFileSync(__dirname + namePath).toString();
     console.log('App Name: ' + nameString);
 
-    var stringsXML = fs.readFileSync(__dirname + '/../../platforms/android/src/main/res/values/strings.xml').toString();
+    var stringsXML = fs.readFileSync(__dirname + xmlPath).toString();
 
     var builder = new xml2js.Builder();
     var parser = new xml2js.Parser();
@@ -21,7 +24,7 @@ module.exports = function($logger) {
 
         stringsXML = builder.buildObject(data);
         
-        fs.writeFileSync(__dirname + '/../../platforms/android/src/main/res/values/strings.xml', stringsXML);
+        fs.writeFileSync(__dirname + xmlPath, stringsXML);
 
         resolve();
       }
